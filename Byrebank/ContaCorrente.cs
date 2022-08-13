50e5ac5c929142e0f33c9435633ae82bf4daa1fe
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2022-08-13 00:47:03 -0300

    1ª Atualização do curso de POO

---
+++ Byrebank/ContaCorrente.cs
@@ -0,0 +1,11 @@
+﻿namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int numero_agencia;
+        public string nome_agencia;
+        public double saldo;
+    }
+}
---
+++ Byrebank/Program.cs
@@ -0,0 +1,33 @@
+﻿using bytebank;
+
+
+Console.WriteLine("Boas Vindas ao seu banco, ByteBank");
+
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "Pablo Assis";
+conta1.conta = "4026-10";
+conta1.numero_agencia = 23 ;
+conta1.nome_agencia = "Bradesco";
+conta1.saldo = 100;
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = " Samuel Assis";
+conta2.conta = "4026-20";
+conta2.numero_agencia = 70;
+conta2.nome_agencia = "Santander";
+conta2.saldo = 1200;
+
+
+Console.WriteLine("Titular: " + conta1.titular);
+Console.WriteLine("conta" + conta1.conta);
+Console.WriteLine("Numero Agencia: " + conta1.numero_agencia);
+Console.WriteLine("Nome da Agencia" + conta1.nome_agencia);
+Console.WriteLine("Salado   : " + conta1.saldo);
+
+Console.WriteLine("Titular: " + conta2.titular);
+Console.WriteLine("conta" + conta2.conta);
+Console.WriteLine("Numero Agencia: " + conta2.numero_agencia);
+Console.WriteLine("Nome da Agencia" +  conta2.nome_agencia);
+Console.WriteLine("Salado   : " + conta2.saldo);
+
+Console.ReadLine();
