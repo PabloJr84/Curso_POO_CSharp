c3b90026c2d76c69c18dc8c520aa30d318278cfd
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2022-08-31 23:33:39 -0300

    Atualizando nome do projeto

--- Byrebank/ContaCorrente.cs
+++ Byrebank/ContaCorrente.cs
@@ -3,11 +3,15 @@
 {
     public class ContaCorrente
     {
-        
+        public string titular_nome;
+        public string titular_cpf;
+        public string titular_Profissao;
+
         public string conta;
         public int numero_agencia;
         public string nome_agencia;
         public double saldo;
+        public string titular;
 
 
 
